6a13752ce97a32884322ac9f618768b2e662add1
RianMetelko <85962407+RianMetelko@users.noreply.github.com>
2021-06-15 15:25:54 -0400

    Security

---
+++ scripts/config_xml.js
@@ -0,0 +1,54 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description tsc:version="2.0.1" dc:date="2019-06-27 11:44:26 AM" dc:source="Camtasia Studio,8.6.0,enu" dc:title="Security Profiles" tscDM:firstFrame="Security_Profiles_First_Frame.png" tscDM:originId="03ABE0E6-5F75-4D4E-B891-B362BC243ED1" tscDM:project="Security Profiles">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="191900"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="1150"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmp:label="1" xmpDM:startTime="73870" xmpDM:duration="5660" tscDM:boundingPoly="1041,612;1134,612;1134,693;1041,693;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="2" xmpDM:startTime="106770" xmpDM:duration="360" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="3" xmpDM:startTime="180630" xmpDM:duration="11270" tscDM:boundingPoly="511,618;679,618;679,710;511,710;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="http://www.hdds.com" tscHS:newWindow="0"/></rdf:li><rdf:li><rdf:Description xmp:label="4" xmpDM:startTime="81470" xmpDM:duration="8800" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="5" xmpDM:startTime="132830" xmpDM:duration="640" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="6" xmpDM:startTime="109730" xmpDM:duration="370" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="7" xmpDM:startTime="120700" xmpDM:duration="370" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="8" xmpDM:startTime="126070" xmpDM:duration="630" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="9" xmpDM:startTime="127130" xmpDM:duration="640" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="10" xmpDM:startTime="133900" xmpDM:duration="630" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="11" xmpDM:startTime="134970" xmpDM:duration="630" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="12" xmpDM:startTime="136030" xmpDM:duration="640" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="13" xmpDM:startTime="140970" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="14" xmpDM:startTime="148500" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="15" xmpDM:startTime="150300" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="16" xmpDM:startTime="155400" xmpDM:duration="600" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="17" xmpDM:startTime="164630" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="18" xmpDM:startTime="167830" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="19" xmpDM:startTime="179530" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="20" xmpDM:startTime="94530" xmpDM:duration="370" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="21" xmpDM:startTime="125270" xmpDM:duration="360" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="22" xmpDM:startTime="139870" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="23" xmpDM:startTime="142230" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="24" xmpDM:startTime="171230" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="25" xmpDM:startTime="176330" xmpDM:duration="1100" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="26" xmpDM:startTime="100830" xmpDM:duration="370" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="27" xmpDM:startTime="138100" xmpDM:duration="630" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li><rdf:li><rdf:Description xmp:label="28" xmpDM:startTime="139230" xmpDM:duration="640" tscDM:boundingPoly="1033,610;1126,610;1126,691;1033,691;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="Introduction" xmpDM:startTime="0" tscDM:image="Security_Profiles_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Profile Concept" xmpDM:startTime="6833" tscDM:image="Security_Profiles_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Demonstration" xmpDM:startTime="79500" tscDM:image="Security_Profiles_Thumbnails.png" tscDM:imageindex="2" tscDM:imageoffset="0" tscDM:imagerect="150, 0, 75, 46"/></rdf:li><rdf:li><rdf:Description xmpDM:name="Resources" xmpDM:startTime="180667" tscDM:image="Security_Profiles_Thumbnails.png" tscDM:imageindex="3" tscDM:imageoffset="0" tscDM:imagerect="225, 0, 75, 46"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="Security Profiles.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="Security_Profiles_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="Security_Profiles_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
